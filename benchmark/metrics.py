bab7960c497a124149fefc6632986ebc2fafe622
Arik Ermshaus <ermshaua@informatik.hu-berlin.de>
2023-02-20 11:44:43 +0100

    Added missing covering metric.

--- benchmark/metrics.py
+++ benchmark/metrics.py
@@ -42,5 +42,98 @@ def f_measure(annotations, predictions, margin=5, alpha=0.5, return_PR=False):
     return F
 
 
+def overlap(A, B):
+    """ Return the overlap (i.e. Jaccard index) of two sets
+
+    >>> overlap({1, 2, 3}, set())
+    0.0
+    >>> overlap({1, 2, 3}, {2, 5})
+    0.25
+    >>> overlap(set(), {1, 2, 3})
+    0.0
+    >>> overlap({1, 2, 3}, {1, 2, 3})
+    1.0
+    """
+    return len(A.intersection(B)) / len(A.union(B))
+
+
+def partition_from_cps(locations, n_obs):
+    """ Return a list of sets that give a partition of the set [0, T-1], as
+    defined by the change point locations.
+
+    >>> partition_from_cps([], 5)
+    [{0, 1, 2, 3, 4}]
+    >>> partition_from_cps([3, 5], 8)
+    [{0, 1, 2}, {3, 4}, {5, 6, 7}]
+    >>> partition_from_cps([1,2,7], 8)
+    [{0}, {1}, {2, 3, 4, 5, 6}, {7}]
+    >>> partition_from_cps([0, 4], 6)
+    [{0, 1, 2, 3}, {4, 5}]
+    """
+    T = n_obs
+    partition = []
+    current = set()
+
+    all_cps = iter(sorted(set(locations)))
+    cp = next(all_cps, None)
+    for i in range(T):
+        if i == cp:
+            if current:
+                partition.append(current)
+            current = set()
+            cp = next(all_cps, None)
+        current.add(i)
+    partition.append(current)
+    return partition
+
+
+def cover_single(Sprime, S):
+    """Compute the covering of a segmentation S by a segmentation Sprime.
+
+    This follows equation (8) in Arbaleaz, 2010.
+
+    >>> cover_single([{1, 2, 3}, {4, 5}, {6}], [{1, 2, 3}, {4, 5, 6}])
+    0.8333333333333334
+    >>> cover_single([{1, 2, 3, 4}, {5, 6}], [{1, 2, 3, 4, 5, 6}])
+    0.6666666666666666
+    >>> cover_single([{1, 2}, {3, 4}, {5, 6}], [{1, 2, 3}, {4, 5, 6}])
+    0.6666666666666666
+    >>> cover_single([{1, 2, 3, 4, 5, 6}], [{1}, {2}, {3}, {4, 5, 6}])
+    0.3333333333333333
+    """
+    T = sum(map(len, Sprime))
+    assert T == sum(map(len, S))
+    C = 0
+    for R in S:
+        C += len(R) * max(overlap(R, Rprime) for Rprime in Sprime)
+    C /= T
+    return C
+
+
+def covering(annotations, predictions, n_obs):
+    """Compute the average segmentation covering against the human annotations.
+
+    annotations : dict from user_id to iterable of CP locations
+    predictions : iterable of predicted Cp locations
+    n_obs : number of observations in the series
+
+    >>> covering({1: [10, 20], 2: [10], 3: [0, 5]}, [10, 20], 45)
+    0.7962962962962963
+    >>> covering({1: [], 2: [10], 3: [40]}, [10], 45)
+    0.7954144620811286
+    >>> covering({1: [], 2: [10], 3: [40]}, [], 45)
+    0.8189300411522634
+
+    """
+    Ak = {
+        k + 1: partition_from_cps(annotations[uid], n_obs)
+        for k, uid in enumerate(annotations)
+    }
+    pX = partition_from_cps(predictions, n_obs)
+
+    Cs = [cover_single(pX, Ak[k]) for k in Ak]
+    return sum(Cs) / len(Cs)
+
+
 def anomaly_match(prediction, true_start, true_end):
     return prediction in range(true_start - 100, true_end + 100)
